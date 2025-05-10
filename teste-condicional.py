03fa52ecfe59e462d6445555f4eacb4eccdc71bf
rmsampa <rminzao0@gmail.com>
2025-05-10 13:57:05 -0300

    teste-condicional simples

---
+++ teste-condicional.py
@@ -0,0 +1,10 @@
+print('Bem-vindo ao DETRAN!')
+print('Este é o novo teste extremamente complexo para liberar ou não a sua CNH')
+print()
+idade = int(input('Digite a sua idade: '))
+if idade >= 18:
+  print('Você pode tirar a CNH')
+else:
+  print('Você não pode tirar a CNH')
+
+print('Acabou')
