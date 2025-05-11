9adc25fceb65111253471a759f1c0f1856cc5a00
rmsampa <rminzao0@gmail.com>
2025-05-11 01:23:10 -0300

    soma-numeros simples

---
+++ soma-numeros.py
@@ -0,0 +1,15 @@
+soma = 0
+n = 1
+
+# while n <= 10:
+#   soma = soma + n
+#   n = n + 1
+  # print(f'Soma: {soma}')
+  # print(f'n: {n}')
+
+for index in range(1, 11):
+  soma += index
+
+# soma = sum([i for i in range(1, 11)])
+  
+print(f'A soma dos números de 1 a 10 é: {soma}')	
